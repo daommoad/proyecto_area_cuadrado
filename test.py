4e6fb0bdfb29a4f423b0eed7089c6f702d8a078f
daommoad <169152295+daommoad@users.noreply.github.com>
2024-05-07 13:06:27 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+def area_cuadrado(lado):
+    area = lado*4
+    print(f"\nEl area resultante de la operaicion es: {area}")
+
+    
+
+
+lado_cua = int(input("Introduce el valor de un lado: "))
+
+area_cuadrado(lado_cua)
